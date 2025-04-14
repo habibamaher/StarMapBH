a738b3228cadfc9eedcf3d6def0abd44f6789ac6
habibamaher <46494748+habibamaher@users.noreply.github.com>
2025-04-14 13:59:45 +0300

    Add files via upload
    This commit is to educational tooltips in which the second funfact is added with a second tooltip.

---
+++ tooltip2.js
@@ -0,0 +1,6 @@
+<p onmouseover="showTooltip2()" onmouseout="hideTooltip2()">
+  Ursa Major
+  <span id="tooltip2" style="display:none; background:#222; color:white; padding:5px; border-radius:5px; position:absolute;">
+    Ursa Major contains the Big Dipper, a well-known asterism.
+  </span>
+</p>
